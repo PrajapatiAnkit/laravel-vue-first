62e843b8ae77eda067e18437248531c9c272e5fd
Ankit <cs.ankitprajapati@gmail.com>
2020-12-27 22:46:24 +0530

    some chnages

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'contact' => '7905266028',
+            'password' => bcrypt(123456)
+        ]);
+    }
+}
